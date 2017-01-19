9e377ce96b6dd7f74eb912a7d388676245e9d6c8
Sonal S Solanki <sonal.singh2288@gmail.com>
2017-01-19 15:54:08 +0530

    Revert "Updated Files as per review"
    This reverts commit 7b1ec9d862ae5c6658cf61df7e4c1a9f08dfcc4d.

--- IndoreCityTourApp/app/src/main/java/com/example/android/indorecitytourapp/HistoricalActivity.java
+++ IndoreCityTourApp/app/src/main/java/com/example/android/indorecitytourapp/HistoricalActivity.java
@@ -14,12 +14,22 @@ public class HistoricalActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
        //Create array lists of object places
         final ArrayList<Places> places = new ArrayList<Places>();
-        places.add(new Places(getResources().getString(R.string.name_historical_rajwada),getResources().getString(R.string.descp_rajwada),R.drawable.meghdootgarden_placename));
-        places.add(new Places(getResources().getString(R.string.name_historical_lalbagh),getResources().getString(R.string.descp_lalbagh),R.drawable.lalbagh_placename));
-        places.add(new Places(getResources().getString(R.string.name_historical_gommat),getResources().getString(R.string.descp_gommatgiri),R.drawable.gommatgiri_placename));
-        places.add(new Places(getResources().getString(R.string.name_historical_phootikothi),getResources().getString(R.string.descp_phootikothi),R.drawable.photikothi_placename));
-        places.add(new Places(getResources().getString(R.string.name_historical_ranisatigate),getResources().getString(R.string.descp_ranisatigate),R.drawable.ranisatigate_placename));
-        places.add(new Places(getResources().getString(R.string.name_historical_krsihnapuri),getResources().getString(R.string.descp_krishnapuri),R.drawable.krishnapurichatri_placename));//Create a adpater for Places object
+        places.add(new Places("Rajwada","Rajwada is a historical palace in Indore city." +
+                " It was built by the Holkars of the Maratha Empire about two centuries ago",R.drawable.rajwada_placename));
+        places.add(new Places("Lal Bagh Palace","Lal Baag Palace was once the resident of the Holkars." +
+                " Know more about Lal Bagh Palace in Indore, Madhya Pradesh.",R.drawable.lalbagh_placename));
+        places.add(new Places("Gommat Giri","A set of jain temples built on the top of Gommatesher Hill, " +
+                "these Jain temples are 24 in number for each of the 24 Tirthankaras.",R.drawable.gommatgiri_placename));
+        places.add(new Places("Phooti Kothi","Historical landmark in Indore, India" +
+                "· Nawa Bangla, Phooti Kothi.",R.drawable.photikothi_placename));
+        places.add(new Places("Rani Sati Gate","Historical landmark in Indore, India",R.drawable.ranisatigate_placename));
+        places.add(new Places("Krishna Puri Chhatri","Chhatri Bagh is one of the most popular tourist attractions in Indore, " +
+                "which is known for its permanent canopies. These canopies are built in the memory of the royal members of" +
+                " the Holkar Dynasty. Each chhatri is built on the burial site of the Holkar king or queen. " +
+                "The chhatri primarily comprises a dome shaped cenotaphs with pyramidal spires on the top. " +
+                "During night, this park draws attention of numerous tourists," +
+                " as the chhatris are illuminated and the fountain at artificial lake starts operating.",R.drawable.chhatribagh_placename));
+        //Create a adpater for Places object
         PlacesAdapter adapter = new PlacesAdapter(this,places,R.color.category_history);
         //Create a listview object and assign list id
         ListView listView = (ListView) findViewById(R.id.list) ;
--- IndoreCityTourApp/app/src/main/java/com/example/android/indorecitytourapp/TemplesActivity.java
+++ IndoreCityTourApp/app/src/main/java/com/example/android/indorecitytourapp/TemplesActivity.java
@@ -16,12 +16,18 @@ public class TemplesActivity extends AppCompatActivity {
         //Create array lists of object places
         final ArrayList<Places> places = new ArrayList<Places>();
 
-        places.add(new Places(getResources().getString(R.string.name_temple_khajrana),getResources().getString(R.string.descp_khajrana),R.drawable.khajranaganeshmandir_placename));
-        places.add(new Places(getResources().getString(R.string.name_temple_kanch),getResources().getString(R.string.descp_kanchtemple),R.drawable.kanchmandir_placename));
-        places.add(new Places(getResources().getString(R.string.name_temple_annpurna),getResources().getString(R.string.descp_annpurna),R.drawable.annpurnamandir_placename));
-        places.add(new Places(getResources().getString(R.string.name_temple_iskon),getResources().getString(R.string.descp_iskon),R.drawable.iskcontemple_placename));
-        places.add(new Places(getResources().getString(R.string.name_temple_ranjeet),getResources().getString(R.string.descp_ranjeet),R.drawable.ranjeethanumanmandir_placename));
-        places.add(new Places(getResources().getString(R.string.name_temple_gajana),getResources().getString(R.string.descp_gajanan),R.drawable.gambhirbhavan_placename));
+        places.add(new Places("Shri Khajrana Ganesh Temple","Khajrana Ganesh Temple is one of the Famous Hindu Temple." +
+                " This Indian Temple is dedicated to Vinayak",R.drawable.khajranaganeshmandir_placename));
+        places.add(new Places("Kanch Mandir","The Kanch Mahal is one of the main tourist attractions in Indore." +
+                " The Kanch Mandir, as the name suggests, is a temple which is entirely made up of glass and mirrors." +
+                " It is primarily a Jain temple and is a wonder in glass.",R.drawable.kanchmandir_placename));
+        places.add(new Places("Annapurna Mandir","Annapurna Mandir is a popular temple, which is dedicated to the Goddess Annapurna." +
+                " It is one of the famous pilgrimage sites in Indore.",R.drawable.annpurnamandir_placename));
+        places.add(new Places("ISKCON","Our Temple invites the public to join us for a day of spirutual " +
+                "contemplation.",R.drawable.iskcontemple_placename));
+        places.add(new Places("Ranjeet Hanuman Mandir","Hanuman Temple",R.drawable.ranjeethanumanmandir_placename));
+        places.add(new Places("Gajanan Maharaj Temple","The Gajanan Maharaj Temples were built to honor Shri Sant Gajanan Maharaj, " +
+                "a late-nineteenth–, early-twentieth-century Indian holy man known to many",R.drawable.gambhirbhavan_placename));
         //Create a adpater for Places object
         PlacesAdapter adapter = new PlacesAdapter(this,places,R.color.category_history);
         //Create a listview object and assign list id
