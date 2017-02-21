22f90658d43b49228e547ab79bf1dc33b0aaaba5
Sonal S Solanki <sonal.singh2288@gmail.com>
2017-02-21 22:30:49 +0530

    Added filter image

---
+++ ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.quakereport;
+
+/**
+ * Created by Ashutosh on 2/21/2017.
+ */
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+
+    }
+}
